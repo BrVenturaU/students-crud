2d41088eb7ac48f573212a0af414c5400eb0b5ac
BrVenturaU <brandonventura16@gmail.com>
2021-05-02 17:26:11 -0600

    Mueve elementos para versionar API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\StudentController;
+use App\Http\Controllers\Api\v1\StudentController as StudentControllerV1;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::resource('students', StudentController::class)->except(['create', 'edit', 'show']);
+Route::prefix('v1')->group(function () {
+    Route::resource('students', StudentControllerV1::class)->except(['create', 'edit', 'show']);
+});
